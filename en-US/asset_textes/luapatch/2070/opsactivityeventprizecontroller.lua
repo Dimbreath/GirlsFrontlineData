350172f55dc2c7350dca8b8da89a0056f44b200a
Dimbreath-Server <dimbreath+1@gmail.com>
2021-02-02 09:57:50 +0000

    [en-US] Client: 20600 | AB: 2021020216 | Data: 12d9220de7f1fb06556d95db67d5123b

--- en-US/asset_textes/luapatch/2070/opsactivityeventprizecontroller.lua
+++ en-US/asset_textes/luapatch/2070/opsactivityeventprizecontroller.lua
@@ -3,17 +3,13 @@ xlua.private_accessible(CS.OPSActivityEventPrizeController)
 
 local Start = function(self)
 	self:Start();
-	self.rightParent.gameObject:SetActive(false);
+	self.rightParent:GetComponent(typeof(CS.UnityEngine.RectTransform)).anchoredPosition = CS.UnityEngine.Vector2(-120,-400);
 	self.transform:Find("SideBar/ScrollRect").gameObject:SetActive(false);
 	self.transform:Find("SideBar/btnShowRewards").gameObject:SetActive(false);
-end
-local InitRight = function(self)
-	self:InitRight();
-	self.rightParent.gameObject:SetActive(false);
-	self.transform:Find("SideBar/ScrollRect").gameObject:SetActive(false);
-	self.transform:Find("SideBar/btnShowRewards").gameObject:SetActive(false);
+	self.transform:Find("Top/HorizontalLayout/Tile").gameObject:SetActive(false);
 end
 
+
 util.hotfix_ex(CS.OPSActivityEventPrizeController,'Start',Start)
-util.hotfix_ex(CS.OPSActivityEventPrizeController,'InitRight',InitRight)
+--util.hotfix_ex(CS.OPSActivityEventPrizeController,'InitRight',InitRight)
 
